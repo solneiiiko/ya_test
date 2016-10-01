6fb829d7acce56807baaa08c74232150e2f49c3c
Oksana Shumilova <solneiiiko@gmail.com>
2016-10-01 23:39:23 +0500

    separator

--- tree/test_tree.rb
+++ tree/test_tree.rb
@@ -2,66 +2,66 @@ require_relative 'tree'
 require 'test/unit'
 
 class TestTree < Test::Unit::TestCase
-	def test_h_2_true
-		tree = {
-			v: 3,
-			l: {v: 2, l: nil , r: nil},
-			r: {v: 5, l: nil , r: nil}
-		}
+  def test_h_2_true
+    tree = {
+      v: 3,
+      l: {v: 2, l: nil , r: nil},
+      r: {v: 5, l: nil , r: nil}
+    }
 
-		assert_true b_tree(tree)
+    assert_true b_tree(tree)
 
-	end
+  end
 
-	def test_h_2_l_false
-		tree = {
-			v: 3,
-			l: {v: 4, l: nil , r: nil},
-			r: {v: 5, l: nil , r: nil}
-		}
-		
-		assert_false b_tree(tree)
-	end
+  def test_h_2_l_false
+    tree = {
+      v: 3,
+      l: {v: 4, l: nil , r: nil},
+      r: {v: 5, l: nil , r: nil}
+    }
+    
+    assert_false b_tree(tree)
+  end
 
-#	 	  3
-#	   / \
+#     3
+#    / \
 #    2   5
 #   / \
 #  1   4
 
-	def test_h_3_r_false
-		tree = {
-			v: 3,
-			l: {v: 2, l: { v:1, l: nil, r: nil } , r: {v:4, l: nil, r: nil}},
-			r: {v: 5, l: nil , r: nil}
-		}
-		assert_false b_tree(tree)
-	end
+  def test_h_3_r_false
+    tree = {
+      v: 3,
+      l: {v: 2, l: { v:1, l: nil, r: nil } , r: {v:4, l: nil, r: nil}},
+      r: {v: 5, l: nil , r: nil}
+    }
+    assert_false b_tree(tree)
+  end
 
-	def test_h_3_r_l_false
-		tree = {
-			v: 3,
-			l: {v: 2, l: { v:1, l: nil, r: nil } , r: nil },
-			r: {v: 5, l: {v:1, l:nil, r:nil} , r: {v:8, l: nil,r: nil}}
-		}
-		assert_false b_tree(tree)
-	end
+  def test_h_3_r_l_false
+    tree = {
+      v: 3,
+      l: {v: 2, l: { v:1, l: nil, r: nil } , r: nil },
+      r: {v: 5, l: {v:1, l:nil, r:nil} , r: {v:8, l: nil,r: nil}}
+    }
+    assert_false b_tree(tree)
+  end
 
-	def test_h_3_r_r_false
-		tree = {
-			v: 3,
-			l: {v: 2, l: { v:1, l: nil, r: nil } , r: nil },
-			r: {v: 5, l: {v:4, l:nil, r:nil} , r: {v:4, l: nil,r: nil}}
-		}
-		assert_false b_tree(tree)
-	end
+  def test_h_3_r_r_false
+    tree = {
+      v: 3,
+      l: {v: 2, l: { v:1, l: nil, r: nil } , r: nil },
+      r: {v: 5, l: {v:4, l:nil, r:nil} , r: {v:4, l: nil,r: nil}}
+    }
+    assert_false b_tree(tree)
+  end
 
-	def test_h_4_true
-		tree = {
-			v: 3,
-			l: {v: 2, l: { v:1, l: nil, r: nil } , r: nil },
-			r: {v: 5, l: {v:4, l:nil, r:nil} , r: {v:8, l: {v: 6, l:nil, r:nil},r: nil}}
-		}
-		assert_true b_tree(tree)
-	end
+  def test_h_4_true
+    tree = {
+      v: 3,
+      l: {v: 2, l: { v:1, l: nil, r: nil } , r: nil },
+      r: {v: 5, l: {v:4, l:nil, r:nil} , r: {v:8, l: {v: 6, l:nil, r:nil},r: nil}}
+    }
+    assert_true b_tree(tree)
+  end
 end

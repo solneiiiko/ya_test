2cef3d577c2fd1133af610729d3d094bec6cd5d4
Oksana Shumilova <solneiiiko@gmail.com>
2016-10-02 00:43:56 +0500

    [separator]

--- bracket/test_bracket.rb
+++ bracket/test_bracket.rb
@@ -2,33 +2,33 @@ require_relative 'bracket'
 require 'test/unit'
 
 class TestBracket < Test::Unit::TestCase
-	def test_false
-		assert_false brackets(11)
-	end
+  def test_false
+    assert_false brackets(11)
+  end
 
-	def test_empty
-		assert_equal brackets(0), ['']
-	end
+  def test_empty
+    assert_equal brackets(0), ['']
+  end
 
-	def test_2
-		assert_equal brackets(2), ['()']
-	end
+  def test_2
+    assert_equal brackets(2), ['()']
+  end
 
-	def test_4
-		assert_equal brackets(4), ['()()', '(())']
-	end
+  def test_4
+    assert_equal brackets(4), ['()()', '(())']
+  end
 
-	def test_6
-		assert_equal brackets(6) - ['()()()', '(())()', '()(())', '((()))', '(()())'], []
-	end
+  def test_6
+    assert_equal brackets(6) - ['()()()', '(())()', '()(())', '((()))', '(()())'], []
+  end
 
-	def test_8
-		res = ['()()()()', '(())()()', '()()(())', '(((())))',
-			'(()())()', '(()())()', '()(()())', '()((()))',
-			'((()))()', '(()()())', '((())())',
-			'((()()))', '(()(()))', '()(())()'
-		]
-		assert_equal brackets(8) - res, []
-	end
+  def test_8
+    res = ['()()()()', '(())()()', '()()(())', '(((())))',
+      '(()())()', '(()())()', '()(()())', '()((()))',
+      '((()))()', '(()()())', '((())())',
+      '((()()))', '(()(()))', '()(())()'
+    ]
+    assert_equal brackets(8) - res, []
+  end
 
 end
